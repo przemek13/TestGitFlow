b779dd2a0a52a6033ea87496af91d3f4a65301f9
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-08-12 10:45:52 +0200

    dodanie metody do sumowania tablicy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,20 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         wyswietlTekst("TEST");
+        int [] testowaTablica = new int [] {3, 4, 5};
+        int wynik = sumowanieTablicy(testowaTablica);
+        wyswietlTekst(Integer.toString(wynik));
     }
 
         public static void wyswietlTekst (String tekst) {
             System.out.println(("Tekst: " + tekst));
+        }
+
+        public static int sumowanieTablicy (int [] tab) {
+            int suma = 0;
+            for (int i = 0; i < tab.length; i++) {
+            suma = suma + tab [i];
+            }
+            return suma;
         }
 }
