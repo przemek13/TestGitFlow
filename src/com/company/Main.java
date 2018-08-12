db91cb08190a311f77bc961cd929c4b7c3459958
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-08-12 10:31:18 +0200

    dodanie metody do wyswietlania tekstu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        wyswietlTekst("TEST");
     }
+
+        public static void wyswietlTekst (String tekst) {
+            System.out.println(("Tekst: " + tekst));
+        }
 }
